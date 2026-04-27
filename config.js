a2c6ee060e22f61855b1ee6d902b0565f6b5bf7b
ishan-x <ishanxbro847@gmail.com>
2026-04-27 15:41:39 +0530

    Change MONGO_URI in config.js
    Updated MONGO_URI with new database credentials.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || '',
     CREDS_JSON: process.env.CREDS_JSON || '',
-    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://chamafreehosting:Chamindu47@21@freechama.moe3veb.mongodb.net/',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://igirathnayake_db_user:vh7YRqw5ROWOPxM9@ishanmadusanke.dobuuxg.mongodb.net/',
     AUTO_VIEW_STATUS: 'true',
     AUTO_LIKE_STATUS: 'true',
     AUTO_RECORDING: 'true',
